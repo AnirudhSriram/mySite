759b19fad713c8d590d9ddbe07e199912c57f2cb
Terrorbladezz <anirudhsriram1995@gmail.com>
2020-12-31 17:37:35 +0530

    init

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,64 +2,64 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
+  title: 'Anirudh Sriram', // e.g: 'Name | Developer'
+  lang: 'en', // e.g: en, es, fr, jp
   description: '', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Anirudh Sriram',
+  subtitle: 'I am a web developer and a JavaScript Enthusiast',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
-  img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  img: 'me.jpg',
+  paragraphOne: 'I am Anirudh Sriram, a self taught web developer. I am a full time web developer at Siemens and a part time open source contributer.',
+  paragraphTwo: 'I spend most of my time building projects to learn new tech and improve my standing on HackerRank.',
+  paragraphThree: 'I Currently work on React for my projects and Angular at work',
+  resume: 'https://docs.google.com/document/d/e/2PACX-1vQopHraQMFR-ohXU1XfrcxBStw3xqBOL7j3QQbjTPqq5ruVtjYmKMKnDxGEJEvqaz93SICr34U86dkl/pub', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
 export const projectsData = [
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'StockWatch.png',
+    title: 'StockWatch',
+    info: 'My CS50 final project to track stocks of interest and allow users to correlate stock performance with news revolving around the stock.',
+    info2: 'StockWatch is built on the Angular platform with database, authentication and hosting on firebase.',
+    url: 'https://cs50-2c42b.web.app',
+    repo: '', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
+    img: 'hackerNewsClone.png',
+    title: 'HackerNews Clone',
+    info: 'A hackernews top 10 news aggregator.' ,
+    info2: 'Built with react and axios for consumption of the hackernews API. Deployed on github pages. Serves as my crisp top 10 that I need',
+    repo: 'https://github.com/AnirudhSriram/hackerNewsClone', 
+    url: 'https://anirudhsriram.github.io/hackerNewsClone/' // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: 'project.jpg',
-    title: '',
-    info: '',
-    info2: '',
-    url: '',
-    repo: 'https://github.com/cobidev/react-simplefolio', // if no repo, the button will not show up
-  },
+    img: 'meme-gen.png',
+    title: 'Meme Generator',
+    info: 'A fun web app for creating completely random memes',
+    info2: 'One of my first React projects, MemeGenerator is a simple, no-frills meme generator.',
+    repo: 'https://github.com/AnirudhSriram/Meme-Generator',
+    url: '', // if no repo, the button will not show up
+  }
 ];
 
 // CONTACT DATA
 export const contactData = {
   cta: '',
   btn: '',
-  email: '',
+  email: 'anirudhsriram1995@gmail.com',
 };
 
 // FOOTER DATA
@@ -68,22 +68,17 @@ export const footerData = {
     {
       id: nanoid(),
       name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
+      url: 'https://twitter.com/anirudhsriramzz',
     },
     {
       id: nanoid(),
       name: 'linkedin',
-      url: '',
+      url: 'https://www.linkedin.com/in/anirudhsriram/',
     },
     {
       id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/AnirudhSriram',
     },
   ],
 };
